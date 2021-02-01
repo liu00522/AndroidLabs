d92be461e470a1e00520c0b32552158c705d8265
liu00522 <liu00522@algonquinlive.com>
2021-01-31 20:06:49 -0500

    Git Gridlayout - Final

--- app/src/main/java/com/cst2335/liu00522/MainActivity.java
+++ app/src/main/java/com/cst2335/liu00522/MainActivity.java
@@ -3,6 +3,11 @@ package com.cst2335.liu00522;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  click button to show toast message
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this,
+                // why not R.string.toast_msg?
+                R.string.toast_message,
+                Toast.LENGTH_LONG).show());
+
+        //  switch on to show Snackbar message
+        Switch mySwitch = findViewById(R.id.mySwitch);
+
+        mySwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Snackbar.make(btn, isChecked ? R.string.isOn : R.string.isOff,
+                    Snackbar.LENGTH_LONG)
+                    .setAction("Undo", click -> buttonView.setChecked(!isChecked))
+                    .show();
+        });
+
     }
+
+
 }
