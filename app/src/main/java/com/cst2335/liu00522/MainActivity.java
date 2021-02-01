5eb015ce9e7b039d69b27ffae680d6aaa74daa13
liu00522 <liu00522@algonquinlive.com>
2021-01-31 20:53:31 -0500

    Git RelativeLayout - Final

--- app/src/main/java/com/cst2335/liu00522/MainActivity.java
+++ app/src/main/java/com/cst2335/liu00522/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_grid);
 
         //  click button to show toast message
         Button btn = findViewById(R.id.button);
